583d08ab4283c9d6166ead8ae038a0bbdc75004a
Kevin Huck <kevin.r.huck@gmail.com>
2020-03-04 22:29:53 -0600

    Open Handler

--- src/main/kotlin/com/huckk/Handler.kt
+++ src/main/kotlin/com/huckk/Handler.kt
@@ -3,7 +3,7 @@ package com.huckk
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 
-class Handler(val service: Service = Service()) : RequestHandler<String, String>{
+open class Handler(val service: Service = Service()) : RequestHandler<String, String>{
     override fun handleRequest(requestBody: String, context: Context): String {
         return service.getHelloWorld().onEach {
             println(it)
